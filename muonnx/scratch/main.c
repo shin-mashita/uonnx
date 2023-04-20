c7f156a74d1c63007865c0100fefb40cfd7355a5
shin-mashita <dakarashin0@gmail.com>
2023-04-20 17:08:13 +0800

    on arena

--- muonnx/scratch/main.c
+++ muonnx/scratch/main.c
@@ -1,146 +1,187 @@
 #include <uonnx.h>
 
-TensorArena * arena_init(int max_tensors, const size_t MAX_BLOCKS) // TODO: Restrict by size not by number of tensors. Add param MAX_BYTES??.
+
+TensorArena * arena_init(const int MAX_TENSORS, const int MAX_BYTES)
 {
-	int i;
-	Tensor * t;
+    int i;
+    TensorArena * arena = malloc(sizeof(arena));
 
-	TensorArena * arena;
-	arena = malloc(sizeof(TensorArena));
-	if(arena)
-	{
-		arena->tensors = malloc(sizeof(Tensor *)*max_tensors);
-		for(i = 0; i < max_tensors; i++)
-		{
-			t = &arena->tensors[i];
-			t = malloc(sizeof(Tensor));
-			t = NULL;
-		}
+    if(arena)
+    {
+        arena->n_tensors = MAX_TENSORS;
+        arena->tensors = malloc(sizeof(Tensor *)*MAX_TENSORS);
+        arena->datas = malloc(sizeof(void *)*MAX_BYTES);
+        memset(arena->datas, 0, sizeof(void *)*MAX_BYTES);
+        
+        for(i=0; i<arena->n_tensors; i++)
+        {
+            arena->tensors[i] = malloc(sizeof(Tensor));
+            arena->tensors[i]->name = NULL;
+        }
 
-		arena->n_tensors = max_tensors;
-		arena->datas = malloc(sizeof(void *)*MAX_BLOCKS);
-		memset(arena->datas, 0, sizeof(arena->datas)*MAX_BLOCKS);
-	}
-	return arena;
+        return arena;
+    }
+
+    return NULL;
 }
 
-void free_arena(TensorArena * arena)
+// Tensor * tensor_malloc()
+// {
+
+// }
+
+Tensor * tensor_init(   const char * name, 
+                        TensorType type, 
+                        int64_t * dims, 
+                        int ndim, 
+                        TensorArena * arena, 
+                        int tensor_idx, 
+                        int data_idx)
 {
-	if(arena)
-	{
-		free(arena->tensors);
-		free(arena->datas);
-		free(arena);
-	}
+    // Check if & needed
+    int i = 0; size_t ndata = 1;
+    Tensor * t;
+
+    if(arena != NULL)
+    {
+        t = arena->tensors[tensor_idx];
+    }
+    else
+    {
+        t = malloc(sizeof(Tensor)); //TODO: Find way to free if arena == NULL
+    }
+    
+
+    t->name = strdup(name); // Free later
+    t->type = type;
+
+    t->dims = malloc(sizeof(int64_t)*ndim); //Free later
+    memcpy(t->dims, dims, sizeof(int64_t)*ndim);
+
+    t->ndim = ndim;
+
+    for(i=0; i<ndim; i++)
+    {
+        ndata*= dims[i];
+    }
+
+    if(ndata != 0)
+    {
+        t->ndata = ndata;
+    }
+
+    t->datas = &arena->datas[data_idx];
+
+    if(t->datas)
+    {
+        memset(t->datas, 0, sizeof(void *)*ndata);
+    }
+
+    return t;
+
 }
 
-void arena_add_tensor(TensorArena * arena)
+
+// Use TensorProto addresses for permanent tensor (weights)
+// Does not memcpy
+Tensor * tensor_init_from_proto(TensorProto * tp, Tensor * tensor) 
 {
+    int i;
+    size_t ndata = 1;
+    Tensor * t;
+    
+    if(tensor == NULL)
+    {
+        t = malloc(sizeof(Tensor));
+    }
+    else
+    {
+        t = tensor;
+    }
+    
+    if(tp)
+    {
+        t->name = tp->name;
+        t->type = tp->data_type;
+        t->dims = tp->dims;
+        t->ndim = tp->n_dims;
 
+        for(i < 0; i < t->ndim; i++)
+        {
+            ndata *= t->dims[i];
+        }
+
+        switch(t->type)
+        {
+            case TENSOR_TYPE_FLOAT32:
+                t->datas = tp->float_data;
+                t->ndata = tp->n_float_data;
+                break;
+            
+            case TENSOR_TYPE_INT64:
+                t->datas = tp->int64_data;
+                t->ndata = tp->n_int64_data;
+                break;
+            
+            default:
+                break; // ADD SUPPORT FOR OTHER DTYPES
+        }
+
+
+        if(ndata != t->ndata) 
+        {
+            return NULL;
+        }
+
+
+        return t;
+    }
+
+    return NULL;
 }
-
-
-Tensor * tensor_init(const char * name, TensorType type, int64_t * dims, size_t ndim, TensorArena * arena, int data_idx, int tensor_idx)
-{
-	int i, ndata = 1;
-	
-	if(ndim)
-	{
-		for(i=0; i < ndim; i++)
-		{
-			ndata *= dims[i];
-		}
-	}
-	
-	Tensor * t = &arena->tensors[tensor_idx];
-	// Tensor * t = malloc(sizeof(Tensor));
-
-	if(t)
-	{
-		// Allocate memory for Tensor name, dims
-
-		t->dims = malloc(sizeof(int64_t)*ndim);
-		t->datas = &arena->datas[data_idx]; // TODO: Change idx based on planner
-		// TODO: Separate dataarena for different dtypes? 
-
-		// t->datas = malloc(sizeof(float)*ndata);
-		// addrcmp(t->datas,t);
-
-		// Populate assigned/initial values for t
-
-		t->name = strdup(name);
-		t->type = type;
-		
-		t->ndim = ndim;
-		memcpy(t->dims, dims, sizeof(int64_t)*ndim);
-		
-		if(ndata)
-		{
-			t->ndata = ndata;
-		}
-
-		switch(t->type)
-		{
-			case TENSOR_TYPE_FLOAT32:
-				memset(t->datas, 0, sizeof(float)*ndata);
-				break;
-			case TENSOR_TYPE_INT64:
-				memset(t->datas, 0, sizeof(int64_t)*ndata);
-				break;
-			default: // TODO: Add more dtypes.
-				break;
-		}
-	}
-
-	return t;
-}
-
-void free_tensor(Tensor * t)
-{
-	if(t)
-	{
-		if(t->name) free(t->name);
-		if(t->dims) free(t->dims);
-		// if(t->datas) free(t->datas);
-
-		free(t);
-	}
-}
-
-// void set_tensor(); // Add values to tensor
 
 int main()
 {
-	ModelProto * model = NULL;
-	GraphProto * graph = NULL;
-	
-	const char * filename = "./scratch/model.onnx";
+    ModelProto * model = NULL;
+    GraphProto * graph = NULL;
 
-	model = load_model(filename);
-	
-	TensorProto * tp = model->graph->initializer[0];
-	TensorProto * tp2 = model->graph->initializer[1];
+    const char * filename = "./scratch/model.onnx";
+    
+    model = load_model(filename);
 
-	TensorArena * arena;
-	arena = arena_init(10, 10000);
+    TensorProto * tp0 = model->graph->initializer[0];
+    TensorProto * tp1 = model->graph->initializer[1];
 
-	Tensor * v, *v2;
-	v = tensor_init(tp->name, tp->data_type, tp->dims, tp->n_dims, arena, 0, 0);
-	dump_tensor(v);
-	addrcmp(v, &arena->tensors[0]);
-	addrcmp(v->datas, &arena->datas[0]);
+    TensorArena * arena = arena_init(10, 10000);
 
-	v2 = tensor_init(tp2->name, tp2->data_type, tp2->dims, tp2->n_dims, arena, 3000, 1);
-	dump_tensor(v2);
+    void * testdata = malloc(sizeof(float)*5);
+    float * p = (float *) testdata;
+    for(int i=0; i<5; i++)
+    {   
+        p[i] = 0.6969 + i;
+    }
 
-	addrcmp(v2, &arena->tensors[1]);
-	addrcmp(v2->datas, &arena->datas[3000]);
+    Tensor * t0 = tensor_init_from_proto(tp0, arena->tensors[1]);
+    Tensor * t1 = tensor_init_from_proto(tp1, arena->tensors[5]);
+    
+    
+    Tensor * t2 = tensor_init(tp0->name, tp0->data_type, tp0->dims, tp0->n_dims, arena, 1, 0);
 
-	// free_tensor(v);
-	// free_tensor(v2);
-	// free_arena(arena);
-	free_model(model);
-	
+    
+    for(int i = 0; i<10; i++)
+    {
+        if(arena->tensors[i]->name == NULL)
+        {
+            printf("%d is null\n",i);
+        }
+    }
 
+    dump_tensor(t0);
+    dump_tensor(t1);
+
+    dump_tensor(t2);
+
+    free_model(model);
+    
     return 0;
 }
